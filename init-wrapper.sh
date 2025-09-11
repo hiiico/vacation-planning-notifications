23d2fadbf2047b66f3dab8c872946382b5c13324
Hristo Ivanov <hiiico332@gmail.com>
2025-09-11 14:43:23 +0300

    Update init-wrapper.sh

--- init-wrapper.sh
+++ init-wrapper.sh
@@ -5,8 +5,8 @@ set -e
 mysql -u root -p$MYSQL_ROOT_PASSWORD <<EOF
 CREATE DATABASE IF NOT EXISTS \`vacation_planning-notifications\`;
 CREATE DATABASE IF NOT EXISTS \`vacation_planning\`;
-CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-GRANT ALL PRIVILEGES ON \`vacation_planning-notifications\`.* TO '${MYSQL_USER}'@'%';
-GRANT ALL PRIVILEGES ON \`vacation_planning\`.* TO '${MYSQL_USER}'@'%';
+CREATE USER IF NOT EXISTS '${DB_USERNAME}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`vacation_planning-notifications\`.* TO '${DB_USERNAME}'@'%';
+GRANT ALL PRIVILEGES ON \`vacation_planning\`.* TO '${DB_USERNAME}'@'%';
 FLUSH PRIVILEGES;
 EOF
